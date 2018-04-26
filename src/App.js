68f660dd89049a0e9fb7bb3b79d58ab356df7abc
Marta Wieczorek <wieczorek.mk@gmail.com>
2018-04-26 16:11:22 +0200

    tworzenie strony kota

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './CatPage/logo.svg';
 import './App.css';
+import CatPage from "./CatPage/CatPage";
+import {
+  BrowserRouter,
+  Route,
+  Link,
+} from 'react-router-dom';
 
 class App extends Component {
   render() {
@@ -13,6 +19,10 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <BrowserRouter>
+        <Route path="/catpage/" component={CatPage} />
+          <Link
+        </BrowserRouter>
       </div>
     );
   }
