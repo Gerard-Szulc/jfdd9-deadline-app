9e60a1c2690cffc003178b0e66b9ad8f7aef314f
EwaK <klebba.ewa@gmail.com>
2018-05-07 16:52:52 +0200

    CatPage

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Main from './Main'
 import FavoriteCats from './FavoriteCats'
 import Shelters from './Shelters'
 import Profil from './Profil'
-import CatPage from './CatPage/CatPage'
+import CatPage from './CatPage/CatPageView'
 import LocationMap from './LocationMap'
 
 class App extends Component {
@@ -19,7 +19,7 @@ class App extends Component {
           <Route exact path="/favorite-cats" component={FavoriteCats} />
           <Route exact path="/shelters" component={Shelters} />
           <Route exact path="/profil" component={Profil} />
-          <Route path="/catpage" component={CatPage}/>
+          <Route path="/catpage/:catId" component={CatPage}/>
           <Route path="/location" component={LocationMap}/>
         <nav>
           <p><Link to="/catpage">Strona kota</Link></p>
--- src/CatPage/CatPageView.js
+++ src/CatPage/CatPageView.js
@@ -1,54 +1,18 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import './CatPage.css';
+import CatPageFetcher from "./CatPageFetcher";
 
 
 class CatPage extends Component {
 
-  state = {
-    cat: null,
-    fetching: false,
-    error: null
-  }
-
-  componentDidMount() {
-    this.setState({
-      fetching: true,
-      error: null
-    })
-    fetch(
-      process.env.PUBLIC_URL + '/cats.json'
-    ).then(
-      response => response.json()
-    ).then(
-      cats => this.setState({
-        cat : cats.find(cat =>
-          cat.id.toString() === this.props.match.params.catId),
-        fetching: false
-      })
-    ).catch(
-      error => this.setState({
-        error,
-        fetching: false
-      })
-    )
+  handleFavoriteClick = event => {
   }
 
   render() {
     return (
-      <div className="CatPage">
-        <div className="catDiv">
-          <button className="catButtons">catFavorite</button>
-        </div>
-        <div className="catDiv">
-          <p className="catDescription">
-            {this.state.cat !== null ?
-              this.state.cat.description : null
-            }
-          </p>
-          <button className="catButtons">adoptButton</button>
-          <button className="catButtons">calendarButton</button>
-        </div>
-      </div>
+      <Fragment>
+        <CatPageFetcher catId = {this.props.match.params.catId}/>
+      </Fragment>
     )
   }
 }
