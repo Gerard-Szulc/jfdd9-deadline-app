bedd5a11b4a017a685d4fd0e2105a938048ddc0f
Gerard Szulc <gerszulc05@gmail.com>
2018-05-08 16:45:34 +0200

    search working

--- src/List.js
+++ src/List.js
@@ -1,33 +1,60 @@
-import React, {Component, Fragment} from 'react'
-import {DataBase} from './CatBase'
-
+import React, {Component} from 'react'
 
 class List extends Component {
 
+  state = {
+    cats: [],
+    fetching: false,
+    error: null,
+  };
+
+  componentDidMount() {
+    this.setState({
+      fetching: true,
+      error: null
+    });
+
+    fetch(
+      process.env.PUBLIC_URL + '/cats.json'
+    ).then(
+      response => response.json()
+    ).then(
+      cats => this.setState({cats})
+    ).catch(
+      error => this.setState({
+        error,
+        fetching: false
+      })
+    )
+  }
 
   render() {
 
     const search = this.props.search.reduce(
       (result, next) => {
-        const key = Object.keys(next)[0]
-        result[key] = next[key]
+        const key = Object.keys(next)[0];
+        result[key] = next[key];
         return result
-      },
-          {}
+      },{}
+    );
 
-    )
-    console.log(search)
+    console.log(search);
     return (
-      DataBase.filter(
-        cat => (
-          (search.selectedSexOption && search.selectedSexOption.includes(cat.sex)) ||
-          (search.selectedRaceOption && search.selectedRaceOption.includes(cat.race)) ||
-          (search.selectedAgeOption && search.selectedAgeOption.includes(cat.age)) ||
-          (search.selectedColorOption && search.selectedColorOption.includes(cat.ointment))
 
-        )
+
+      // this.state.cats.map(cat=>(<div>{cat.name}</div>))
+
+
+      this.state.cats.filter(
+       cat => (
+         (search.selectedSexOption.length !== 0 ? search.selectedSexOption.includes(cat.sex) : true) &&
+         (search.selectedRaceOption.length !==0 ? search.selectedRaceOption.includes(cat.race) : true) &&
+         (search.selectedAgeOption.length !==0 ? search.selectedAgeOption.includes(cat.age) : true) &&
+         (search.selectedColorOption.length !==0 ? search.selectedColorOption.includes(cat.ointment) : true)
+       )
       ).map(
-        cat => <p>{cat.name} : {cat.sex}</p>
+       cat =>(<div>{cat.name} {cat.id} </div>)
+         //<CatContainer cat={cat}/>)
       )
     )
 
