91e7aa239701e32860db0db1ba65a433e5500d29
Marta Wieczorek <wieczorek.mk@gmail.com>
2018-05-09 16:27:38 +0200

    zmiana w buttonie zmiana we flexie

--- src/CatContainer.js
+++ src/CatContainer.js
@@ -9,6 +9,7 @@ class CatContainer extends Component{
           <img src={this.props.cat.image} alt={this.props.cat.name} />
         </div>
         <div className="catDescription">
+        <p><strong>{this.props.cat.name}</strong> <em>- {this.props.cat.sex.toLowerCase()}</em></p>
           <p>{this.props.cat.description.split(' ').slice(0,20).join(' ')}...<Link to={`/catpage/${this.props.cat.id}`}>
                                                                                                  <button>Pokaż więcej</button>
                                                                                                </Link></p>
