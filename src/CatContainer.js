ae448bf605dd511160d686f4df0f04c4b70f04f8
Gerard Szulc <gerszulc05@gmail.com>
2018-05-09 16:20:54 +0200

    styling dla strony kotka

--- src/CatContainer.js
+++ src/CatContainer.js
@@ -11,11 +11,13 @@ class CatContainer extends Component{
         <div className="catContainerDescription">
           <p>{this.props.cat.description}</p>
         <div className="catDescription">
-          <p>{this.props.cat.description.split(' ').slice(0,20).join(' ')}...<Link to={`/catpage/${this.props.cat.id}`}>
-                                                                                                 <button>Pokaż więcej</button>
-                                                                                               </Link></p>
+          <p>{this.props.cat.description.split(' ').slice(0,20).join(' ')}...
+            <Link to={`/catpage/${this.props.cat.id}`}>
+             <button>Pokaż więcej</button>
+             </Link></p>
         </div>
 
+      </div>
       </div>
     )
   }
