6ee3cb6353fcc55cf1c5d4427cb0198aa0161e1a
Gerard Szulc <gerszulc05@gmail.com>
2018-05-11 16:35:59 +0200

    Przyciski na stronie kota działają jak trzeba

--- src/CatPage/context/CatPageContext.js
+++ src/CatPage/context/CatPageContext.js
@@ -22,41 +22,13 @@ export class CatPageProvider extends Component {
       })
     },
 
-        // this.setState(
-        //
-        //  this.state.favourite.every(
-        //    element =>{
-        //      return(element.id !== cat.id)}) ? {favButtonName: "Polubiłeś mnie"} : {favButtonName: "Polub mnie"  }))
-
-    // removeFavourite: (cat) => {
-    // //
-    //   this.setState({
-    //     favourite: (this.state.favourite.filter(element => element.id !== cat.id))},
-    //     this.setState({
-    //       favButtonName: "Polub mnie"
-    // //
-    //     }))
-    // },
-
     toggleCatAdopted: (cat) => {
       this.setState( {
         adopted: this.state.adopted.includes(cat.id) ? this.state.adopted.filter(catId => catId !== cat.id) : this.state.adopted.concat(cat.id) }
-        // this.setState(
-        //   this.state.adopted.every(element => {return (element.id !== cat.id)})  ? {adoptButtonName: 'Adoptowałeś mnie'} : {adoptButtonName: 'Adoptuj mnie'}
-        // )
 
         );
 
     },
-    // removeAdopted: (cat) => {
-    //
-    //   this.setState({
-    //     adopted: (this.state.adopted.filter(element => element.id !== cat.id))},
-    //     this.setState({
-    //       adoptButtonName: 'Adoptuj mnie'
-    //     })
-    //   )
-    // },
 
   };
 
