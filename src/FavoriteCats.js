89bd89cb8fd7233d90292ac7a7b243c75e7fc558
Michal Maniara <maniara.michal@gmail.com>
2018-05-11 16:23:05 +0200

    zamiana favorite na ulubione

--- src/FavoriteCats.js
+++ src/FavoriteCats.js
@@ -6,7 +6,7 @@ class FavoriteCats extends Component{
   render() {
     return(
       <div>
-        <h1> FavoriteCats</h1>
+        <h1>Ulubione koty</h1>
 
         {this.props.cats && this.props.cats.filter( cat => this.props.favourite.includes(cat.id) ).map(element=><CatContainer key={element.id} cat={element}/>)}
 
