b2f7ac8a5b9ce02606dc7ddf8ff8e6d69c8a2585
Marta Wieczorek <wieczorek.mk@gmail.com>
2018-05-17 15:29:44 +0200

    zaimportowanie obrazka hamburgera do menu zaciągnięcie css ze strony

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,41 +1,42 @@
 import React, {Component} from 'react';
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 import './navBar.css';
 import logo from './logo-white.png';
+import hamburger from './hamburger.png'
 
 
 class NavBar extends Component {
   render() {
-    return(
-        <nav>
-          <div className="logo">
-            <Link to="/"><img src={logo} alt="logo" /></Link>
-          </div>
-          <ul>
-            <label for="show-menu" class="show-menu">
-              <li id="hamburger" class="hamburger">
-                <figure><img src="./hamburger.png" alt="hamburger" /></figure>
+    return (
+      <nav>
+        <div className="logo">
+          <Link to="/"><img src={logo} alt="logo"/></Link>
+        </div>
+        <ul>
+          <label for="show-menu" className="show-menu">
+            <li id="hamburger" className="hamburger">
+              <figure><img src={hamburger} alt="hamburger"/></figure>
 
-              </li>
-            </label>
-            <input type="checkbox" id="show-menu" role="button" />
-            <li>
-              <Link to="/">Wyszukiwanie</Link>
             </li>
-            <li>
-              <Link to="/favorite-cats">Ulubione Koty</Link>
+          </label>
+          <input type="checkbox" id="show-menu" role="button"/>
+          <li className="right-side">
+            <Link to="/">Wyszukiwanie</Link>
+          </li>
+          <li className="right-side">
+            <Link to="/favorite-cats">Ulubione Koty</Link>
+          </li>
+          <li class="right-side">
+            <Link to="/shelters">Schroniska</Link>
+          </li>
+          <div className="menuRight">
+            <li className="right-side">
+              <Link to="/profile">Twój Profil</Link>
             </li>
-            <li>
-              <Link to="/shelters">Schroniska</Link>
-            </li>
-            <div className="menuRight">
-              <li>
-                <Link to="/profile">Twój Profil</Link>
-              </li>
-            </div>
-          </ul>
-          <div className="clear"></div>
-        </nav>
+          </div>
+        </ul>
+        <div className="clear"></div>
+      </nav>
     )
   }
 }
