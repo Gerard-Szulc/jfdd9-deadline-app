adcb2a8138073a6563180d76261ad8ce93f33dc3
Michal Maniara <maniara.michal@gmail.com>
2018-05-18 16:34:26 +0200

    dodanie info window

--- src/Shelters.js
+++ src/Shelters.js
@@ -2,25 +2,51 @@ import React, { Component } from 'react'
 
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
-const markers = {
+const getMarkers = context => ({
   'Promyk':  <Marker
     key={'Promyk'}
     title={'Promyk'}
     name={'Promyk'}
+    onClick={context.onClick}
     position={{lat: 54.374760, lng: 18.449328}} />,
   'Ciapkowo': <Marker
     key={'Ciapkowo'}
     title={'Ciapkowo'}
     name={'Ciapkowo'}
+    onClick={context.onClick}
     position={{lat: 54.492730, lng: 18.527670}} />,
   'Schronisko Sopot': <Marker
     key={'Schronisko Sopot'}
     title={'Schronisko Sopot'}
     name={'Schronisko Sopot'}
+    onClick={context.onClick}
     position={{lat: 54.443741, lng: 18.552334}} />
-}
+})
 
 class Shelters extends Component {
+
+  state = {
+    showingInfoWindow: false,
+    activeMarker: null,
+    selectedPlace: null
+  }
+
+  onClick = (props, marker, e) => {
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+  }
+
+  // // onClick = (props) => {
+  // //   if (this.state.showingInfoWindow) {
+  // //     this.setState({
+  // //       showingInfoWindow: false,
+  // //       activeMarker: null
+  // //     })
+  // //   }
+  // };
 
   render() {
     return (
@@ -33,15 +59,20 @@ class Shelters extends Component {
            gestureHandling={this.props.gestureHandling || 'greedy'}
            zoom={12}
            onClick={this.onMapClicked}
+           position={'centre'}
 
       >
 
-        {this.props.shelter ? markers[this.props.shelter] :
-          Object.values(markers)
+        {this.props.shelter ? getMarkers(this)[this.props.shelter] :
+          Object.values(getMarkers(this))
         }
 
-        <InfoWindow onClose={this.onInfoWindowClose}>
+        <InfoWindow onOpen={this.onInfoWindowOpen}
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+        >
           <div>
+            <h1>Tu znajdziesz kota dla siebie</h1>
           </div>
         </InfoWindow>
       </Map>
