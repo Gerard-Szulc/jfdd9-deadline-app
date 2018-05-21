266aaa3ad43b346d3860dd9e7f7421c0da26c2b3
Marta Wieczorek <wieczorek.mk@gmail.com>
2018-05-21 15:29:12 +0200

    zmiana dzaiłania favoriteCats

--- src/FavoriteCats.js
+++ src/FavoriteCats.js
@@ -2,23 +2,29 @@ import React, {Component} from 'react';
 import {withCatPage} from "./CatPage/context/CatPageContext";
 import CatContainer from "./List/CatContainer";
 import './FavoriteCats/FavoriteCats.css'
+import {withUser} from "./User/context/User";
+
 
 class FavoriteCats extends Component{
   render() {
     return(
       <div className="favoriteCats">
         <h1>Twoje ulubione koty</h1>
-        { this.props.fetching === false ? (this.props.cats && this.props.cats
-          .filter( cat =>  this.props.favourite.some((favouriteCat)=> favouriteCat.catId === cat.id))
-          .map(element =>
-            <CatContainer key={element.id} cat={element}/>
-          )): 'Loading'}
+        {/*{ this.props.fetching === false ? (this.props.cats && this.props.cats*/}
+          {/*.filter( cat =>  this.props.favourite.some((favouriteCat)=> favouriteCat.catId === cat.id))*/}
+          {/*.map(element =>*/}
+            {/*<CatContainer key={element.id} cat={element}/>*/}
+          {/*)): 'Loading'}*/}
 
-        {this.props.favourite.length === 0 ? <p>Nie polubiłeś jeszcze żadnego kota. </p> : <p> </p>}
+        {/*{this.props.favourite.length === 0 ? <p>Nie polubiłeś jeszcze żadnego kota. </p> : <p> </p>}*/}
 
+
+        {this.props.cats && this.props.cats.filter( cat =>
+          this.props.favourite.some(request => request.catId === cat.id && request.user === this.props.user.uid)
+        ).map(element=><CatContainer key={element.id} cat={element}/>)}
       </div>
     )
   }
 }
 
-export default withCatPage(FavoriteCats);
+export default withUser(withCatPage(FavoriteCats));
