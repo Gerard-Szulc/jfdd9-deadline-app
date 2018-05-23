2217909eea7126e6dd1f7770b92ef113ae85cb53
Marta Wieczorek <wieczorek.mk@gmail.com>
2018-05-23 16:12:36 +0200

    nie ładzia, ale zainstalowana już paczka

--- src/List/List.js
+++ src/List/List.js
@@ -1,13 +1,16 @@
 import React, {Component} from 'react'
 import CatContainer from "./CatContainer";
 import {withCatPage} from "../CatPage/context/CatPageContext";
-import './List.css'
+import './List.css';
+import Pagination from 'react-js-pagination'
+
 
 class List extends Component {
 
   state = {
     fetching: false,
     error: null,
+    activePage: 1,
   };
 
   componentDidMount() {
@@ -17,10 +20,17 @@ class List extends Component {
     });
 
   }
+
 componentDidUpdate(){
 
 }
+
+  handlePageChange(pageNumber) {
+    this.setState({activePage: pageNumber});
+  }
+
   render() {
+
 
     const search = this.props.search.reduce(
       (result, next) => {
@@ -35,19 +45,29 @@ componentDidUpdate(){
 
 <div className={"list"}>{
 
-  this.props.cats ? (this.props.cats.filter(
-    cat => (
-      (search.selectedSexOption.length !== 0 ? search.selectedSexOption.includes(cat.sex) : true) &&
-      (search.selectedRaceOption.length !== 0 ? search.selectedRaceOption.includes(cat.race) : true) &&
-      (search.selectedAgeOption.length !== 0 ? search.selectedAgeOption.includes(cat.age) : true) &&
-      (search.selectedColorOption.length !== 0 ? search.selectedColorOption.includes(cat.ointment) : true) &&
-      (this.props.fetching === false) &&
-      (!this.props.adoptionRequests.some(adoptedCat=> adoptedCat.catId === cat.id && adoptedCat.accepted !== false ))
-    )
+
+  <Pagination
+    activePage={this.state.activePage}
+    itemsCountPerPage={5}
+    totalItemsCount={this.props.cats.length}
+    pageRangeDisplayed={5}
+    onChange={this.handlePageChange}
+  />}
+
+{this.props.cats ? (this.props.cats.filter(
+  cat => (
+  (search.selectedSexOption.length !== 0 ? search.selectedSexOption.includes(cat.sex) : true) &&
+  (search.selectedRaceOption.length !== 0 ? search.selectedRaceOption.includes(cat.race) : true) &&
+  (search.selectedAgeOption.length !== 0 ? search.selectedAgeOption.includes(cat.age) : true) &&
+  (search.selectedColorOption.length !== 0 ? search.selectedColorOption.includes(cat.ointment) : true) &&
+  (this.props.fetching === false) &&
+  (!this.props.adoptionRequests.some(adoptedCat=> adoptedCat.catId === cat.id && adoptedCat.accepted !== false ))
+  )
   ).map(
-    cat => (<CatContainer cat={cat} key={cat.id}/>)
-  )) : 'ŁADOWANIE KOTÓW'
-}</div>
+  cat => (<CatContainer cat={cat} key={cat.id}/>)
+  )) : 'ŁADOWANIE KOTÓW'}
+
+</div>
     )
 
   }
