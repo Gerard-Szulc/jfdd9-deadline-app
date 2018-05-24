641a63381d7b3b1386a2aec7e2b4aa6f35de2a43
EwaK <klebba.ewa@gmail.com>
2018-05-24 16:28:08 +0200

    zzz

--- src/User/UserProfile.js
+++ src/User/UserProfile.js
@@ -4,8 +4,9 @@ import SignInForm from './SignInForm';
 import SignUpForm from './SignUpForm';
 import {withCatPage} from "./../CatPage/context/CatPageContext";
 import CatContainer from "./../List/CatContainer";
+import {Link} from "react-router-dom";
 import styleCss from './css/style-form.css';
-import firebase from 'firebase'
+import {sheltersUid} from './../config/shelters-users-Id';
 
 
 class UserProfile extends Component {
@@ -13,6 +14,13 @@ class UserProfile extends Component {
     console.log(this.props.adoptionRequests)
     return (
       <div className="UserView">
+
+        {/*{console.log(this.props.user && this.props.user.uid)}*/}
+
+        <div className="shelter-panel">
+          <Link to="/add-cat">Dodaj kota do adopcji</Link>
+        </div>
+
         {
           this.props.user === null ? (
             <div className="if-no-user-container">
@@ -20,15 +28,20 @@ class UserProfile extends Component {
               <SignUpForm />
             </div>
 
-          ) : (
-            <div>
-              <h2>Twoje koty</h2>
-              {              this.props.cats && this.props.cats.filter( cat =>
-                this.props.adoptionRequests.some(request => request.catId === cat.id && request.user === this.props.user.uid)
-              ).map(element=><CatContainer key={element.id} cat={element}/>)}
+          ) : {
 
-            </div>
-            )
+            Object.values(sheltersUid).find(val => val === this.props.user.uid) === 'undefined' ? 'nie możesz' : 'możesz dodać kota'
+            }
+
+            // (
+            // <div>
+            //   <h2>Twoje koty</h2>
+            //   {              this.props.cats && this.props.cats.filter( cat =>
+            //     this.props.adoptionRequests.some(request => request.catId === cat.id && request.user === this.props.user.uid)
+            //   ).map(element=><CatContainer key={element.id} cat={element}/>)}
+            //
+            // </div>
+            // )
         }
       </div>
     )
